f69799b3a152d029dfe1d6eac4623c8c88b97dc8
Darwin Froese <dfroese@korewireless.com>
2018-09-06 14:54:36 -0500

    Changing build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+echo "Setup..."
 mkdir /out /artifacts
-
 cd /go/src/github.com/dfroese-korewireless/continuous-demo
+
+echo "Building..."
 go get -d -v ./...
 go build -o /out/demo .
-tar -C /out -zcvf /artifacts/app.tar.gz *
+
+echo "Extracting..."
+cd /out
+tar -zcvf /artifacts/app.tar.gz *
