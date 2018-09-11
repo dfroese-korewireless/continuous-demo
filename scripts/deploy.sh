d62ffcc8a389402e189c2b9b40297e26e708c70c
Darwin Froese <dfroese@korewireless.com>
2018-09-11 16:51:43 -0500

    Playing around with traefik labels

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,11 @@
 docker stop demo_1
 docker rm demo_1
 
-docker run -d --hostname demo.k1d-dockerh-01.ksg.int -l traefik.domain=demo.k1d-dockerh-01.ksg.int -l traefik.backend=demo -l traefik.enable=true -l traefik.docker.network=traefik_proxy -l traefik.frontend.entryPoints=http,https -l traefik.port=80 --network internal --network traefik_proxy --name demo_1 continuous-demo
+docker run -d --hostname demo.k1d-dockerh-01.ksg.int -l traefik.domain=demo.k1d-dockerh-01.ksg.int -l traefik.backend=demo -l traefik.enable=true -l traefik.frontend.rule=Host:demo.k1d-dockerh-01.ksg.int -l traefik.docker.network=traefik_proxy -l traefik.frontend.entryPoints=http,https -l traefik.port=80 --network internal --network traefik_proxy --name demo_1 continuous-demo
 
 # sleep 2m
 
 docker stop demo_2
 docker rm demo_2
 
-docker run -d --hostname demo.k1d-dockerh-01.ksg.int -l traefik.backend=demo -l traefik.enable=true -l traefik.frontend.rule=Host:demo.k1d-dockerh-01.ksg.int -l traefik.docker-network=traefik_proxy -l traefik.frontend.entryPoints=http,https -l traefik.port=80 --network internal --network traefik_proxy --name demo_2 continuous-demo
+docker run -d -l traefik.backend=demo --network internal --network traefik_proxy --name demo_2 continuous-demo
